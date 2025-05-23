9406f1155ff29235e1c5a8bc06cf291b60d22bc3
Jonathan B. Coe <jonathanbcoe@gmail.com>
2025-05-23 12:45:16 +0300

    Add support for multiple levels

--- src/xyz/human_resource_machine/__main__.py
+++ src/xyz/human_resource_machine/__main__.py
@@ -1,38 +1,41 @@
-from xyz.human_resource_machine.interpreter import (
-    Interpreter,
-    Label,
-    Inbox,
-    Outbox,
-    CopyTo,
-    CopyFrom,
-    Jump,
-)
+import os
+import sys
+from typing import Any
+from xyz.human_resource_machine.interpreter import Interpreter, Value
+
+import argparse
+import importlib.util
+
+
+def _load_level(level: int) -> Any:
+    module_name = f"xyz.human_resource_machine.challenges.level_{level}"
+    file_path = os.path.join(
+        os.path.dirname(__file__), "challenges", f"level_{level}.py"
+    )
+    if not os.path.exists(file_path):
+        print(f"Module path {file_path} not found.")
+        return
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    assert spec is not None, f"Level {level} not found"
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)  # type: ignore
+    return module
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Human Resource Machine Interpreter")
+    parser.add_argument("level", type=int, help="Level number to execute")
+    args = parser.parse_args()
+
+    level = _load_level(args.level)
+
     interpreter = Interpreter()
-    interpreter.registers = {
-        0: "N",
-        1: "K",
-        2: "A",
-        3: "E",
-        4: "R",
-        5: "D",
-        6: "O",
-        7: "L",
-        8: "Y",
-        9: "J",
-        12: 8,
-    }
-    interpreter.instructions = [
-        Label("BEGIN"),
-        Inbox(),
-        CopyTo(12),
-        CopyFrom(12, indirect=True),
-        Outbox(),
-        Jump("BEGIN"),
-    ]
-    input = [6, 2, 0, 3, 4]
+
+    interpreter.registers = level.REGISTERS
+    interpreter.instructions = level.INSTRUCTIONS
+    input: list[Value] = level.INPUT
+
     interpreter.execute_program(input)
     print(interpreter.to_str(), "\n")
     print("Registers:", interpreter.registers)
